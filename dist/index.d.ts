8f771ae6aa3b0f64396fbe08b219b2898659667b
reallyGoodBaker <79316659+reallyGoodBaker@users.noreply.github.com>
2023-12-31 20:32:06 +0800

    update
    Optimized the import statement

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,4 @@
-export * from './decorators/index';
+export * from './decorators';
 export * from './server';
 export * from './pool';
+export * from './plugins';
--- dist/plugins/index.js
+++ dist/plugins/index.js
@@ -14,7 +14,4 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./decorators"), exports);
-__exportStar(require("./server"), exports);
-__exportStar(require("./pool"), exports);
-__exportStar(require("./plugins"), exports);
+__exportStar(require("./util"), exports);
